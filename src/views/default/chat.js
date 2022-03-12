83ee00190b0cd599f54a3d32d9ec0e9a0a527282
Ömer ERDOĞAN <omer.erdogan@ydyazilim.com>
2022-03-12 22:37:58 +0300

    commit

---
+++ src/views/default/chat.js
@@ -0,0 +1,33 @@
+'use strict';
+/* global io, location, alert, document, Vue */
+
+// Config
+var host = location.origin;
+var url = parseInt(host.split(':').reverse()[0]);
+var hostSocket = host.replace(url, 8001);
+
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/chat.html');
+});
+
+var userId = 0;
+io.on('connection', function(socket){
+    socket.userId = userId ++;
+    console.log('a user connected, user id: ' + socket.userId);
+
+    socket.on('chat', function(msg){
+        console.log('message from user#' + socket.userId + ": " + msg);
+        io.emit('chat', {
+            id: socket.userId,
+            msg: msg
+        });
+    });
+});
+
+http.listen(8001, function(){
+    console.log('listening on *:8001');
+});
