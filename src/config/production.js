ef69d97c12959dac9187140e8cde0d114e0a2306
Ömer ERDOĞAN <omer.erdogan@ydyazilim.com>
2022-03-12 20:40:11 +0300

    commit

--- src/config/production.js
+++ src/config/production.js
@@ -9,6 +9,7 @@ export default {
   server: {
     // Express
     ip: "game.fourasteknoloji.net",
+    port: 8080,
   },
   log: false, // show logs
   // Roles: if a user has multiple roles, will take the time of the greater role
