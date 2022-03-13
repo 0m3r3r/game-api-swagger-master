1f1770dade70671d8f40f7619953ddc29c5a82c4
Ömer ERDOĞAN <omer.erdogan@ydyazilim.com>
2022-03-13 21:39:58 +0300

    swagger def

--- src/api/sockets/example.socket.js
+++ src/api/sockets/example.socket.js
@@ -13,7 +13,7 @@ export function on() {
     socket.on('game:play', data => emit('play', data));
     socket.on('example:add', data => emit('add', data));
     socket.on('example:delete', data => emit('delete', data));
-    socket.to('game:player').emit(data => data);
+    socket.to('game:player', data => io.to("player").emit("basicEmit", 1, "2", data));
 }
 
 // Emit events
