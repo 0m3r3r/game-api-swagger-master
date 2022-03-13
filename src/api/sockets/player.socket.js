d253d926227b8476175e67932a30e16151405a1f
Ömer ERDOĞAN <omer.erdogan@ydyazilim.com>
2022-03-13 22:21:34 +0300

    swagger def

--- src/api/sockets/player.socket.js
+++ src/api/sockets/player.socket.js
@@ -1,29 +1,19 @@
-export let socket = null;
-export let io = null;
-
-// Constructor
-export default (_socket, _io) => {
-    socket = _socket;
-    io = _io;
-    on();
-}
-
-// Here should be all events 'on'
-export function on() {
-    socket.on('player:play', data => emit('play', data));
-    socket.on("player:another", sock => {
-        sock.on("private message", (anotherSocketId, msg) => {
-            sock.to(anotherSocketId).emit("private message", socket.id, msg);
-        });
-    });
-    socket.on("player:room", (sck) => {
-        console.log(sck.rooms); // Set { <socket.id> }
-        sck.join("room");
-        console.log(sck.rooms); // Set { <socket.id>, "room1" }
-    });
-}
-
-// Emit events
-export function emit(event, data) {
-    io.emit(event, data);
-}
+// export let socket = null;
+// export let io = null;
+//
+// // Constructor
+// export default (_socket, _io) => {
+//     socket = _socket;
+//     io = _io;
+//     on();
+// }
+//
+// // Here should be all events 'on'
+// export function on() {
+//
+// }
+//
+// // Emit events
+// export function emit(event, data) {
+//     io.emit(event, data);
+// }
