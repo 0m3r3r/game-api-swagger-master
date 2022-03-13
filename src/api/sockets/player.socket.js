b03d04236902fb01aee1cc4272f142e6c0a0b826
Ömer ERDOĞAN <omer.erdogan@ydyazilim.com>
2022-03-13 22:57:59 +0300

    swagger def

--- src/api/sockets/player.socket.js
+++ src/api/sockets/player.socket.js
@@ -16,10 +16,10 @@ export function on() {
             console.log(data.anotherSocketId, data.msg);
             socket.to(data.anotherSocketId).emit("player", socket.id, data.msg);
     });
-    socket.on("player:room", (socket) => {
-        console.log(socket.rooms); // Set { <socket.id> }
+    socket.on("player:room", (data) => {
+        console.log(data.rooms); // Set { <socket.id> }
         socket.join("room");
-        console.log(socket.rooms); // Set { <socket.id>, "room1" }
+        console.log(data.rooms); // Set { <socket.id>, "room1" }
     });
 }
 
