f6f9afc37e3868fe4ba32137e23cf98ebe3f1e5a
Ömer ERDOĞAN <omer.erdogan@ydyazilim.com>
2022-03-13 22:29:04 +0300

    swagger def

--- src/api/sockets/example.socket.js
+++ src/api/sockets/example.socket.js
@@ -14,18 +14,6 @@ export function on() {
     socket.on('example:add', data => emit('add', data));
     socket.on('example:delete', data => emit('delete', data));
     socket.to('example:player', data => emit('player', data));
-
-    socket.on('player:play', data => emit('play', data));
-    socket.on("player:another", sock => {
-        sock.on("private message", (anotherSocketId, msg) => {
-            sock.to(anotherSocketId).emit("private message", sock.id, msg);
-        });
-    });
-    socket.on("player:room", (sck) => {
-        console.log(sck.rooms); // Set { <socket.id> }
-        sck.join("room");
-        console.log(sck.rooms); // Set { <socket.id>, "room1" }
-    });
 }
 
 // Emit events
--- src/api/sockets/player.socket.js
+++ src/api/sockets/player.socket.js
@@ -1,19 +1,29 @@
-// export let socket = null;
-// export let io = null;
-//
-// // Constructor
-// export default (_socket, _io) => {
-//     socket = _socket;
-//     io = _io;
-//     on();
-// }
-//
-// // Here should be all events 'on'
-// export function on() {
-//
-// }
-//
-// // Emit events
-// export function emit(event, data) {
-//     io.emit(event, data);
-// }
+export let socket = null;
+export let io = null;
+
+// Constructor
+export default (_socket, _io) => {
+    socket = _socket;
+    io = _io;
+    on();
+}
+
+// Here should be all events 'on'
+export function on() {
+    socket.on('player:play', data => emit('play', data));
+    io.on("player:another", socket => {
+        socket.on("private message", (anotherSocketId, msg) => {
+            socket.to(anotherSocketId).emit("private message", socket.id, msg);
+        });
+    });
+    io.on("player:room", (socket) => {
+        console.log(socket.rooms); // Set { <socket.id> }
+        socket.join("room1");
+        console.log(socket.rooms); // Set { <socket.id>, "room1" }
+    });
+}
+
+// Emit events
+export function emit(event, data) {
+    io.emit(event, data);
+}
