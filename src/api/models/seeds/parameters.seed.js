a1c5b18efdebd0b7e409fe981b5a4a06da6b12e0
Ömer ERDOĞAN <omer.erdogan@ydyazilim.com>
2022-03-20 16:13:43 +0300

    swagger def

--- src/api/models/seeds/parameters.seed.js
+++ src/api/models/seeds/parameters.seed.js
@@ -1,504 +1,80 @@
 export default [
-   "params" [
+    {params: [
         {
-            "windRot": 78.36659,
-            "windStrenght": 8.972946,
-            "time": 5.39257574
+            windRot: 78.36659,
+            windStrenght: 8.972946,
+            time: 5.39257574
         },
         {
-            "windRot": 314.81073,
-            "windStrenght": 11.5258484,
-            "time": 2.19438076
+            windRot: 314.81073,
+            windStrenght: 11.5258484,
+            time: 2.19438076
         },
         {
-            "windRot": 241.1817,
-            "windStrenght": 3.55091953,
-            "time": 9.911553
+            windRot: 241.1817,
+            windStrenght: 3.55091953,
+            time: 9.911553
         },
         {
-            "windRot": 250.614456,
-            "windStrenght": 23.227253,
-            "time": 7.50286436
+            windRot: 250.614456,
+            windStrenght: 23.227253,
+            time: 7.50286436
         },
         {
-            "windRot": 273.163239,
-            "windStrenght": 22.9247684,
-            "time": 6.452762
+            windRot: 273.163239,
+            windStrenght: 22.9247684,
+            time: 6.452762
         },
         {
-            "windRot": 223.104446,
-            "windStrenght": 21.5993881,
-            "time": 7.301056
+            windRot: 223.104446,
+            windStrenght: 21.5993881,
+            time: 7.301056
         },
         {
-            "windRot": 339.597778,
-            "windStrenght": 4.90350533,
-            "time": 8.233974
+            windRot: 339.597778,
+            windStrenght: 4.90350533,
+            time: 8.233974
         },
         {
-            "windRot": 299.753967,
-            "windStrenght": 20.5744572,
-            "time": 9.194287
+            windRot: 299.753967,
+            windStrenght: 20.5744572,
+            time: 9.194287
         },
         {
-            "windRot": 215.361786,
-            "windStrenght": 14.95206,
-            "time": 9.321969
+            windRot: 215.361786,
+            windStrenght: 14.95206,
+            time: 9.321969
         },
         {
-            "windRot": 275.780182,
-            "windStrenght": 9.034767,
-            "time": 6.680941
+            windRot: 275.780182,
+            windStrenght: 9.034767,
+            time: 6.680941
         },
         {
-            "windRot": 176.542755,
-            "windStrenght": 8.017093,
-            "time": 2.30717
+            windRot: 176.542755,
+            windStrenght: 8.017093,
+            time: 2.30717
         },
         {
-            "windRot": 73.7625961,
-            "windStrenght": 22.3483124,
-            "time": 6.10296
+            windRot: 73.7625961,
+            windStrenght: 22.3483124,
+            time: 6.10296
         },
         {
-            "windRot": 179.874237,
-            "windStrenght": 22.8261967,
-            "time": 3.53014374
+            windRot: 179.874237,
+            windStrenght: 22.8261967,
+            time: 3.53014374
         },
         {
-            "windRot": 315.578857,
-            "windStrenght": 10.4031935,
-            "time": 8.453742
+            windRot: 315.578857,
+            windStrenght: 10.4031935,
+            time: 8.453742
         },
         {
-            "windRot": 293.80423,
-            "windStrenght": 15.8562641,
-            "time": 4.4774394
-        },
-        {
-            "windRot": 25.2813,
-            "windStrenght": 23.2831,
-            "time": 9.871429
-        },
-        {
-            "windRot": 317.3044,
-            "windStrenght": 15.6414642,
-            "time": 4.43255568
-        },
-        {
-            "windRot": 110.452057,
-            "windStrenght": 20.7702236,
-            "time": 9.596656
-        },
-        {
-            "windRot": 220.6424,
-            "windStrenght": 18.60864,
-            "time": 8.68413448
-        },
-        {
-            "windRot": 157.612457,
-            "windStrenght": 19.0261383,
-            "time": 9.63048649
-        },
-        {
-            "windRot": 184.082382,
-            "windStrenght": 11.845253,
-            "time": 6.335071
-        },
-        {
-            "windRot": 228.823288,
-            "windStrenght": 15.4400053,
-            "time": 4.3093257
-        },
-        {
-            "windRot": 155.745819,
-            "windStrenght": 6.883133,
-            "time": 7.17853642
-        },
-        {
-            "windRot": 9.5857,
-            "windStrenght": 15.2606583,
-            "time": 2.253869
-        },
-        {
-            "windRot": 71.37285,
-            "windStrenght": 17.0647221,
-            "time": 7.69090366
-        },
-        {
-            "windRot": 44.7422066,
-            "windStrenght": 13.8243685,
-            "time": 2.143115
-        },
-        {
-            "windRot": 32.1175,
-            "windStrenght": 13.7797194,
-            "time": 9.227459
-        },
-        {
-            "windRot": 356.384521,
-            "windStrenght": 9.545972,
-            "time": 6.53586531
-        },
-        {
-            "windRot": 230.296951,
-            "windStrenght": 20.26063,
-            "time": 4.0481143
-        },
-        {
-            "windRot": 286.6348,
-            "windStrenght": 21.53598,
-            "time": 8.519578
-        },
-        {
-            "windRot": 61.9222069,
-            "windStrenght": 22.2680454,
-            "time": 2.146164
-        },
-        {
-            "windRot": 240.281815,
-            "windStrenght": 3.94163227,
-            "time": 9.991289
-        },
-        {
-            "windRot": 325.436584,
-            "windStrenght": 21.8938236,
-            "time": 4.45016336
-        },
-        {
-            "windRot": 302.214264,
-            "windStrenght": 12.4109545,
-            "time": 7.139864
-        },
-        {
-            "windRot": 336.244934,
-            "windStrenght": 3.46310616,
-            "time": 2.010273
-        },
-        {
-            "windRot": 186.481613,
-            "windStrenght": 1.15608311,
-            "time": 7.53467751
-        },
-        {
-            "windRot": 11.5241861,
-            "windStrenght": 21.7464733,
-            "time": 6.895058
-        },
-        {
-            "windRot": 155.468109,
-            "windStrenght": 1.74157143,
-            "time": 4.4787364
-        },
-        {
-            "windRot": 40.7602234,
-            "windStrenght": 2.459199,
-            "time": 3.446313
-        },
-        {
-            "windRot": 351.080231,
-            "windStrenght": 17.1446381,
-            "time": 9.611606
-        },
-        {
-            "windRot": 269.831116,
-            "windStrenght": 23.3215332,
-            "time": 2.59037113
-        },
-        {
-            "windRot": 97.4586639,
-            "windStrenght": 12.5317059,
-            "time": 3.13964176
-        },
-        {
-            "windRot": 101.060638,
-            "windStrenght": 15.0142365,
-            "time": 3.813099
-        },
-        {
-            "windRot": 18.4720078,
-            "windStrenght": 7.161941,
-            "time": 3.33950138
-        },
-        {
-            "windRot": 298.7606,
-            "windStrenght": 8.011879,
-            "time": 7.32272339
-        },
-        {
-            "windRot": 221.106567,
-            "windStrenght": 20.2215672,
-            "time": 6.9583354
-        },
-        {
-            "windRot": 246.3449,
-            "windStrenght": 3.58659649,
-            "time": 5.4273324
-        },
-        {
-            "windRot": 285.039032,
-            "windStrenght": 21.8616486,
-            "time": 5.395469
-        },
-        {
-            "windRot": 286.329163,
-            "windStrenght": 19.211771,
-            "time": 4.997372
-        },
-        {
-            "windRot": 184.170486,
-            "windStrenght": 12.9361668,
-            "time": 5.949571
-        },
-        {
-            "windRot": 257.825531,
-            "windStrenght": 15.4660854,
-            "time": 3.264185
-        },
-        {
-            "windRot": 263.9947,
-            "windStrenght": 21.0119286,
-            "time": 6.56384039
-        },
-        {
-            "windRot": 283.163452,
-            "windStrenght": 7.02886343,
-            "time": 6.077659
-        },
-        {
-            "windRot": 323.441742,
-            "windStrenght": 6.620677,
-            "time": 7.172888
-        },
-        {
-            "windRot": 336.1041,
-            "windStrenght": 8.290139,
-            "time": 6.89929152
-        },
-        {
-            "windRot": 174.954163,
-            "windStrenght": 4.220948,
-            "time": 3.62443066
-        },
-        {
-            "windRot": 202.748154,
-            "windStrenght": 17.31833,
-            "time": 7.707344
-        },
-        {
-            "windRot": 296.70752,
-            "windStrenght": 12.28123,
-            "time": 6.62631559
-        },
-        {
-            "windRot": 126.588829,
-            "windStrenght": 24.49352,
-            "time": 5.19540739
-        },
-        {
-            "windRot": 313.696,
-            "windStrenght": 23.3533115,
-            "time": 3.82241058
-        },
-        {
-            "windRot": 330.204865,
-            "windStrenght": 8.356312,
-            "time": 8.750484
-        },
-        {
-            "windRot": 255.391266,
-            "windStrenght": 4.38237,
-            "time": 2.69562149
-        },
-        {
-            "windRot": 258.5919,
-            "windStrenght": 11.6202593,
-            "time": 4.70192575
-        },
-        {
-            "windRot": 184.130371,
-            "windStrenght": 10.9121647,
-            "time": 4.35697031
-        },
-        {
-            "windRot": 279.3762,
-            "windStrenght": 8.1257515,
-            "time": 6.09650564
-        },
-        {
-            "windRot": 170.851456,
-            "windStrenght": 17.2654362,
-            "time": 6.40468645
-        },
-        {
-            "windRot": 125.098251,
-            "windStrenght": 3.35493088,
-            "time": 2.044344
-        },
-        {
-            "windRot": 206.405746,
-            "windStrenght": 16.5863647,
-            "time": 9.321055
-        },
-        {
-            "windRot": 228.956833,
-            "windStrenght": 2.53733635,
-            "time": 7.041015
-        },
-        {
-            "windRot": 149.290268,
-            "windStrenght": 14.0081682,
-            "time": 5.301472
-        },
-        {
-            "windRot": 342.158325,
-            "windStrenght": 6.04595757,
-            "time": 2.88880348
-        },
-        {
-            "windRot": 122.98175,
-            "windStrenght": 16.3396072,
-            "time": 4.02522039
-        },
-        {
-            "windRot": 17.30939,
-            "windStrenght": 3.950513,
-            "time": 2.41497231
-        },
-        {
-            "windRot": 35.6684875,
-            "windStrenght": 5.984769,
-            "time": 4.030004
-        },
-        {
-            "windRot": 45.31101,
-            "windStrenght": 9.68693352,
-            "time": 4.343715
-        },
-        {
-            "windRot": 260.18045,
-            "windStrenght": 21.9524689,
-            "time": 4.54965639
-        },
-        {
-            "windRot": 124.987015,
-            "windStrenght": 17.9721432,
-            "time": 8.939626
-        },
-        {
-            "windRot": 89.5318756,
-            "windStrenght": 5.170124,
-            "time": 4.009708
-        },
-        {
-            "windRot": 269.632263,
-            "windStrenght": 2.96335125,
-            "time": 3.4590683
-        },
-        {
-            "windRot": 39.2849655,
-            "windStrenght": 17.9218864,
-            "time": 9.815319
-        },
-        {
-            "windRot": 44.5874977,
-            "windStrenght": 2.37340546,
-            "time": 5.760178
-        },
-        {
-            "windRot": 284.657654,
-            "windStrenght": 17.8228512,
-            "time": 6.714702
-        },
-        {
-            "windRot": 245.68988,
-            "windStrenght": 18.1576385,
-            "time": 2.81439018
-        },
-        {
-            "windRot": 270.9755,
-            "windStrenght": 14.25139,
-            "time": 7.405965
-        },
-        {
-            "windRot": 218.253,
-            "windStrenght": 4.377672,
-            "time": 8.590339
-        },
-        {
-            "windRot": 105.879517,
-            "windStrenght": 18.35202,
-            "time": 7.202259
-        },
-        {
-            "windRot": 305.219177,
-            "windStrenght": 18.2133026,
-            "time": 7.080993
-        },
-        {
-            "windRot": 163.832047,
-            "windStrenght": 12.73098,
-            "time": 8.763076
-        },
-        {
-            "windRot": 317.6719,
-            "windStrenght": 10.0512714,
-            "time": 6.02035475
-        },
-        {
-            "windRot": 224.883972,
-            "windStrenght": 23.8278923,
-            "time": 4.36377859
-        },
-        {
-            "windRot": 164.795074,
-            "windStrenght": 17.3723755,
-            "time": 8.936881
-        },
-        {
-            "windRot": 127.0067,
-            "windStrenght": 6.22403336,
-            "time": 5.22045
-        },
-        {
-            "windRot": 113.035179,
-            "windStrenght": 23.2988129,
-            "time": 5.80288649
-        },
-        {
-            "windRot": 200.553162,
-            "windStrenght": 18.0167179,
-            "time": 5.78818
-        },
-        {
-            "windRot": 85.42016,
-            "windStrenght": 20.918066,
-            "time": 5.77871943
-        },
-        {
-            "windRot": 318.991516,
-            "windStrenght": 10.1912746,
-            "time": 8.492657
-        },
-        {
-            "windRot": 305.859344,
-            "windStrenght": 24.1659451,
-            "time": 8.766683
-        },
-        {
-            "windRot": 120.0687,
-            "windStrenght": 5.942377,
-            "time": 5.46325636
-        },
-        {
-            "windRot": 87.10695,
-            "windStrenght": 21.1586,
-            "time": 4.87020159
-        },
-        {
-            "windRot": 52.33273,
-            "windStrenght": 16.5247688,
-            "time": 4.42149
+            windRot: 293.80423,
+            windStrenght: 15.8562641,
+            time: 4.4774394
         }
     ]
+    }
 ];
