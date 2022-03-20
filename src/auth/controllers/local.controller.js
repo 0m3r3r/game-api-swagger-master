0ac19c547c8600b9b18832748bd7ffff6768159b
Ömer ERDOĞAN <omer.erdogan@ydyazilim.com>
2022-03-20 14:15:48 +0300

    swagger def

--- src/auth/controllers/local.controller.js
+++ src/auth/controllers/local.controller.js
@@ -3,5 +3,5 @@ import { initialize } from '../services/session.service';
 
 // Callback passport
 export function callback(req, res, next) {
-  passport.authenticate('local', (err, user) => initialize(err, user, res))(req, res, next);
+  passport.authenticate('password', (err, user) => initialize(err, user, res))(req, res, next);
 }
